3c6341b7a806d33871f3c65f4bc986a5dca55f98
Shurtugl_fromENI <shurtugl@hotmail.fr>
2023-02-23 15:04:23 +0100

    [css] pa : neon text et neon div et state image

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -209,7 +209,8 @@ class Event implements JsonSerializable
         return $this;
     }
 
-    private function getUsersAsArray(){
+    private function getUsersAsArray(): array
+    {
         $usersAsArray = [];
         foreach($this->users as $user){
             $usersAsArray[] = $user->getEmail();
@@ -217,10 +218,13 @@ class Event implements JsonSerializable
         return $usersAsArray;
     }
 
-    /*
+    /**
      * ajouter-retirer un utilisateur
+     * @param $applyUser
+     * @return $this|null
      */
-    public function apply($applyUser){
+    public function apply($applyUser): ?static
+    {
         $alreadyin=false;
         foreach($this->users as $user){
             if ($applyUser->getId() == $user->getId()){
@@ -237,6 +241,11 @@ class Event implements JsonSerializable
         return $this;
     }
 
+    /**
+     * transforme les infos d'un objet en texte json
+     * @return mixed
+     *
+     */
     public function jsonSerialize(): mixed
     {
         $tool = new toolKitBQP();
@@ -255,6 +264,9 @@ class Event implements JsonSerializable
         ]);
     }
 
+    /**
+     * @return int|null Indique le nombre de places restantes. Null si aucune limite
+     */
     public function getRoom(){
         if($this->peopleMax == null){
             return null;
@@ -262,6 +274,9 @@ class Event implements JsonSerializable
         return $this->peopleMax - $this->users->count();
     }
 
+    /**
+     * @return string etat actuel calculé à partir des dates et du booléen "ispublished"
+     */
     public function getState(): string
     {
         $today=new DateTime('now');
